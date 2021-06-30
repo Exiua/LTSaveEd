faf5966e190de7076ed4d5be68d15540eb58e31b
Exiua <master6969697@gmail.com>
2021-06-30 00:49:53 -0500

    Tested modifying textField text and currently working on reading data between the core tag for the PlayerCharacter from the xml save file

--- src/LTSaveEd/Controller.java
+++ src/LTSaveEd/Controller.java
@@ -2,9 +2,13 @@ package LTSaveEd;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -17,9 +21,10 @@ import java.util.Properties;
 public class Controller {
 
     private Stage stage;
+    private AnchorPane root;
     private final Properties prop;
     private File workingFile;
-    private Document xml;
+    private Document saveFile;
 
     /**
      * Creates a new Controller object and parses config.ini
@@ -50,6 +55,15 @@ public class Controller {
     }
 
     /**
+     * Sets root to the root node of the fxml file
+     * @param r
+     *   Root node of fxml file
+     */
+    public void setRoot(AnchorPane r){
+        root = r;
+    }
+
+    /**
      * Opens a file selector so that the user can select their save .xml file they want to edit
      * @param event
      *   Action event performed
@@ -67,15 +81,18 @@ public class Controller {
         fc.setInitialDirectory(new File(currentPath));
         File f = fc.showOpenDialog(stage);
         if(f != null) {
-            prop.setProperty("defaultFilePath", f.getParent());
-            prop.store(new FileOutputStream("config.ini"), null);
+            //Update config.ini if a different folder was used
+            if(!f.getParent().equals(currentPath)) {
+                prop.setProperty("defaultFilePath", f.getParent());
+                prop.store(new FileOutputStream("config.ini"), null);
+            }
             workingFile = f;
             //Load XML
             DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
             try (InputStream is = new FileInputStream(f)){
                 DocumentBuilder db = dbf.newDocumentBuilder();
-                xml = db.parse(is);
-                System.out.println("Working?");
+                saveFile = db.parse(is);
+                System.out.println(f);
             }
             catch(ParserConfigurationException | SAXException e){
                 e.printStackTrace();
@@ -84,7 +101,46 @@ public class Controller {
     }
 
     @FXML
+    private void selectCharacter(ActionEvent event){
+        event.consume();
+        setFields("");
+    }
+
+    private void setFields(String id){
+        id = "PlayerCharacter"; //Placeholder; will change to take input from combobox to select character
+        Node idNode = getElementByIdValue(id);
+        System.out.println(idNode.getNodeValue());
+        Node characterNode = idNode.getParentNode().getParentNode();
+        System.out.println(characterNode);
+        NodeList attributeNodes = characterNode.getChildNodes();
+        NodeList coreNodes = attributeNodes.item(0).getChildNodes();
+        for(int i = 0; i < coreNodes.getLength(); i++){
+            System.out.println(coreNodes.item(i).getNodeValue());
+        }
+    }
+
+    private Node getElementByIdValue(String id){
+        NodeList nodes = saveFile.getElementsByTagName("id");
+        for(int i = 0 ; i < nodes.getLength(); i++){
+            Node currNode = nodes.item(i);
+            NamedNodeMap attributes = currNode.getAttributes();
+            Node value = attributes.getNamedItem("value");
+            if(value != null && value.getTextContent().equals(id)){
+                return currNode;
+            }
+        }
+        return null;
+    }
+
+    @FXML
     private void saveFile(ActionEvent event){
         //TODO: Write saveFile method
     }
+
+    /*@FXML
+    private void lookupTest(ActionEvent event){
+        event.consume();
+        TextField tf = (TextField) root.lookup("#core3");
+        tf.setText("25.234");
+    }*/
 }
