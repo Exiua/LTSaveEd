46840b5ccccbeca8ea9bea4a4e9e7e3561c704cf
Exiua <master6969697@gmail.com>
2021-06-29 21:34:27 -0500

    Made controller into an instance variable (may be reverted in the future)

--- src/LTSaveEd/LTSaveEd.java
+++ src/LTSaveEd/LTSaveEd.java
@@ -8,11 +8,13 @@ import javafx.stage.Stage;
 
 public class LTSaveEd extends Application {
 
+    private Controller controller;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
         FXMLLoader loader = new FXMLLoader(getClass().getResource("LTSaveEd.fxml"));
         Parent root = loader.load();
-        Controller controller = loader.getController();
+        controller = loader.getController();
         controller.setStage(primaryStage);
         primaryStage.setTitle("LTSaveEd v1.0.0");
         primaryStage.setScene(new Scene(root, 1024, 700));
