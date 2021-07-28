30f1baeb7f82a9df768623ce2a3ea71e86355540
Exiua <master6969697@gmail.com>
2021-07-28 14:27:11 -0500

    Added PerkNode class to help with organizing the perk tree

---
+++ src/LTSaveEd/PerkNode.java
@@ -0,0 +1,90 @@
+package LTSaveEd;
+
+import java.util.ArrayList;
+
+public class PerkNode {
+
+    private final String row;
+    private final String name;
+    private final String type;
+    private boolean active;
+    private final ArrayList<PerkNode> parent;
+    private final ArrayList<PerkNode> children;
+
+    public PerkNode(PerkNode perkParent, String perkRow, String perkType, String perkName){
+        parent = new ArrayList<>();
+        if(perkParent != null) {
+            parent.add(perkParent);
+            parent.get(0).addChild(this);
+        }
+        row = perkRow;
+        type = perkType;
+        name = perkName;
+        active = false;
+        children = new ArrayList<>();
+    }
+
+    public PerkNode(PerkNode perkParent1, PerkNode perkParent2, String perkRow, String perkType, String perkName){
+        parent = new ArrayList<>();
+        parent.add(perkParent1);
+        parent.get(0).addChild(this);
+        parent.add(perkParent2);
+        parent.get(1).addChild(this);
+        row = perkRow;
+        type = perkType;
+        name = perkName;
+        active = false;
+        children = new ArrayList<>();
+    }
+
+    public String getRow(){
+        return row;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    private void addChild(PerkNode child){
+        children.add(child);
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean value){
+        if(!value){
+            active = false;
+            deactivate();
+        }
+        else{
+            if(parent.size() == 0){
+                active = true;
+            }
+            else if(parent.size() == 1 && parent.get(0).isActive()){
+                active = true;
+            }
+            else if(parent.get(0).isActive() || parent.get(1).isActive()){
+                active = true;
+            }
+        }
+    }
+
+    private void deactivate(){
+        if(children.size() == 0){
+            return;
+        }
+        for (PerkNode child : children) {
+            child.setActive(false);
+        }
+    }
+
+    private boolean equals(String perkRow, String perkType){
+        return row.equals(perkRow) && type.equals(perkType);
+    }
+}
