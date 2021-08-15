f4c9b6a6ab7fb39f93ba1201a2fbbef5810a5661
Exiua <master6969697@gmail.com>
2021-08-15 10:45:00 -0500

    Added Javadocs

--- src/main/java/LTSaveEd/Launcher.java
+++ src/main/java/LTSaveEd/Launcher.java
@@ -1,5 +1,9 @@
 package LTSaveEd;
 
+/**
+ * Class to launch the program so that it can be packaged into a .jar file
+ * @author Exiua
+ */
 public class Launcher {
 
     public static void main(String[] args){
