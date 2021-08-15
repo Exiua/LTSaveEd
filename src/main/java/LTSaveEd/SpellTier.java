9cf270aabc3f43bc8c18018f9b6c3fd90072e47b
Exiua <master6969697@gmail.com>
2021-08-15 10:43:44 -0500

    Added Javadocs

--- src/main/java/LTSaveEd/SpellTier.java
+++ src/main/java/LTSaveEd/SpellTier.java
@@ -1,19 +1,48 @@
 package LTSaveEd;
 
-import java.util.Arrays;
-
+/**
+ * Class that stores spell attributes to make adding and removing spells and spell upgrades easier
+ * @author Exiua
+ */
 public class SpellTier extends Attribute{
 
+    /**
+     * Base value of a spell (e.g. FIREBALL_1 would have a type of FIREBALL)
+     */
     private final String type;
 
+    /**
+     * Tier of the spell
+     * <p>
+     *     -1: Unowned
+     *     0: Base spell
+     *     1: Upgrade 1
+     *     2: Upgrade 2
+     *     3: Upgrade 3 or upgrade 3A or 3B in the case of elemental spells
+     *     4: Upgrade 3B in the case of steal spell
+     */
     private final int tier;
 
+    /**
+     * Constructor that takes the name and value and parses type and tier from value
+     * @param name
+     *   Display name of the spell
+     * @param value
+     *   Actual value of the spell
+     */
     public SpellTier(String name, String value){
         super(name, value);
         type = readType(value);
         tier = readTier(value);
     }
 
+    /**
+     * Gets the last number in a String
+     * @param str
+     *   String to get the last number from
+     * @return
+     *   Last number in the String as a char
+     */
     private static char getLastNum(String str){
         char num = str.charAt(str.length() - 1);
         if(num == 'A' || num == 'B'){
@@ -22,12 +51,26 @@ public class SpellTier extends Attribute{
         return num;
     }
 
+    /**
+     * Gets the type from a String
+     * @param str
+     *   String to read type from
+     * @return
+     *   String representing the type of the spell
+     */
     public static String readType(String str){
         return str.replace("_UNOWNED", "").replace("_3A", "")
                .replace("_3B", "").replace("_3", "").replace("_2", "")
                .replace("_1", "");
     }
 
+    /**
+     * Gets the tier from a String
+     * @param value
+     *   String to read tier from
+     * @return
+     *   Integer representing the tier of the String
+     */
     public static int readTier(String value){
         String valueType = readType(value);
         if(value.endsWith("UNOWNED")){
@@ -52,10 +95,20 @@ public class SpellTier extends Attribute{
         }
     }
 
+    /**
+     * Gets the type of this spell
+     * @return
+     *   Type of this spell
+     */
     public String getType(){
         return type;
     }
 
+    /**
+     * Gets the tier of this spell
+     * @return
+     *   Tier of this spell
+     */
     public int getTier(){
         return tier;
     }
