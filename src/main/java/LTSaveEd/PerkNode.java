7fd776a3c24d865bd7ab44aa84586e1f6985dda9
Exiua <master6969697@gmail.com>
2021-08-15 03:17:01 -0500

    Cleaned up unused methods; Added static instance variables to help with Node generation and deletion; Modified setActive and added a few more helper methods

--- src/main/java/LTSaveEd/PerkNode.java
+++ src/main/java/LTSaveEd/PerkNode.java
@@ -1,52 +1,60 @@
 package LTSaveEd;
 
+import javafx.collections.ObservableMap;
+import javafx.scene.control.CheckBox;
+import org.w3c.dom.*;
+
 import java.util.ArrayList;
 
 public class PerkNode {
 
+    private static ObservableMap<String, Object> namespace;
+    private static Document saveFile;
+    private static Node perksNode;
     private final String row;
     private final String name;
     private final String type;
-    private boolean active;
     private final ArrayList<PerkNode> parent;
     private final ArrayList<PerkNode> children;
+    private final String id;
+    private boolean active;
+
+    public PerkNode(String perkRow, String perkType, String perkName){
+        parent = new ArrayList<>();
+        row = perkRow;
+        type = perkType;
+        name = perkName;
+        active = false;
+        children = new ArrayList<>();
+        id = "perks$" + row + "$" + type;
+    }
 
     public PerkNode(PerkNode perkParent, String perkRow, String perkType, String perkName){
-        parent = new ArrayList<>();
+        this(perkRow, perkType, perkName);
         if(perkParent != null) {
             parent.add(perkParent);
             parent.get(0).addChild(this);
         }
-        row = perkRow;
-        type = perkType;
-        name = perkName;
-        active = false;
-        children = new ArrayList<>();
+
     }
 
     public PerkNode(PerkNode perkParent1, PerkNode perkParent2, String perkRow, String perkType, String perkName){
-        parent = new ArrayList<>();
-        parent.add(perkParent1);
-        parent.get(0).addChild(this);
+        this(perkParent1, perkRow, perkType, perkName);
         parent.add(perkParent2);
         parent.get(1).addChild(this);
-        row = perkRow;
-        type = perkType;
-        name = perkName;
-        active = false;
-        children = new ArrayList<>();
+
     }
 
-    public String getRow(){
-        return row;
+    public static void setNamespace(ObservableMap<String, Object> namespace){
+        PerkNode.namespace = namespace;
     }
 
-    public String getName() {
-        return name;
+    public static void setPerksNode(Node perksNode){
+        PerkNode.perksNode = perksNode;
     }
 
-    public String getType() {
-        return type;
+    public static void setSaveFile(Document saveFile){
+        PerkNode.saveFile = saveFile;
     }
 
     private void addChild(PerkNode child){
@@ -58,17 +66,45 @@ public class PerkNode {
     }
 
     public void setActive(boolean value){
+        CheckBox cb = (CheckBox) namespace.get(id);
+        cb.setSelected(value);
+        System.out.println("Setting active: " + value);
         if(!value){
             active = false;
+            Node perk = getPerkNode();
+            if(perk != null){
+                perk.getParentNode().removeChild(perk);
+                System.out.println("Removed node: " + this);
+            }
             deactivate();
         }
         else{
-            if(active = true){
-                return;
-            }
             active = true;
+            Element perk = createPerkNode();
+            perksNode.appendChild(perk);
+            System.out.println("Created node: " + this);
             activate();
         }
+    }
+
+    private Node getPerkNode(){
+        NodeList perks = perksNode.getChildNodes();
+        for(int i = 0; i < perks.getLength(); i++){
+            if(perks.item(i).getNodeType() == Node.ELEMENT_NODE){
+                Element perk = (Element) perks.item(i);
+                if(perk.getAttribute("row").equals(row) && perk.getAttribute("type").equals(type)){
+                    return perks.item(i);
+                }
+            }
+        }
+        return null;
+    }
+
+    private Element createPerkNode(){
+        Element perk = saveFile.createElement("perk");
+        perk.setAttribute("row", row);
+        perk.setAttribute("type", type);
+        return perk;
     }
 
     private void activate(){
@@ -77,6 +113,7 @@ public class PerkNode {
         }
         for (PerkNode perkNode : parent) {
             perkNode.setActive(true);
+            System.out.println(perkNode);
         }
     }
 
@@ -84,12 +121,31 @@ public class PerkNode {
         if(children.size() == 0){
             return;
         }
-        for (PerkNode child : children) {
-            child.setActive(false);
+        for (PerkNode perkNode : children) {
+            if(!perkNode.canBeActive()){
+                perkNode.setActive(false);
+            }
         }
     }
 
-    private boolean equals(String perkRow, String perkType){
-        return row.equals(perkRow) && type.equals(perkType);
+    private boolean canBeActive(){
+        for(PerkNode perkNode : parent){
+            if(perkNode.isActive()){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean equals(String perkRow, String perkType){
+        return ("" + row).equals(perkRow) && type.equals(perkType);
+    }
+
+    @Override
+    public String toString(){
+        return "PerkNode{" +
+                "row=" + row +
+                ", type='" + type + '\'' +
+                '}';
     }
 }
