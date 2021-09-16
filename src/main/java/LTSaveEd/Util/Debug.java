8cf4cb0353d0ed5c762d540ef473961791b4235a
Exiua <master6969697@gmail.com>
2021-09-15 21:38:27 -0500

    Removed main and loadXml

--- src/main/java/LTSaveEd/Util/Debug.java
+++ src/main/java/LTSaveEd/Util/Debug.java
@@ -1,18 +1,9 @@
 package LTSaveEd.Util;
 
-import LTSaveEd.Objects.NpcCharacter;
-import org.w3c.dom.Document;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 
 public class Debug {
@@ -134,32 +125,5 @@ public class Debug {
                 System.out.print(nodes.get(i) + ", ");
             }
         }
-    }
-
-    public static void main(String[] args){
-        Document file = loadXml();
-        assert file != null;
-        NodeList nl = file.getElementsByTagName("NPC");
-        if(nl == null){
-            System.out.println("Failed to load file");
-            return;
-        }
-        for(int i = 0; i < nl.getLength(); i++){
-            NpcCharacter nc = new NpcCharacter(nl.item(i));
-            System.out.println(nc.getId());
-        }
-    }
-
-    public static Document loadXml(){
-        //Load XML
-        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-        try (InputStream is = new FileInputStream("D:\\Documents\\Games\\Extra\\Lilith's Throne\\Lilith's Throne (exe version)\\data\\saves\\Save Editor Test.xml")){
-            DocumentBuilder db = dbf.newDocumentBuilder();
-            return db.parse(is);
-        }
-        catch(ParserConfigurationException | SAXException | IOException e){
-            e.printStackTrace();
-        }
-        return null;
     }
 }
