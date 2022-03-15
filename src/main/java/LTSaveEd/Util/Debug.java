79a68d00416fb76601167152d8d5cf69affeb630
Exiua <master6969697@gmail.com>
2022-03-15 15:23:18 -0500

    Testing new version string comparison

--- src/main/java/LTSaveEd/Util/Debug.java
+++ src/main/java/LTSaveEd/Util/Debug.java
@@ -8,6 +8,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 
@@ -148,7 +149,7 @@ public class Debug {
         }
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void versionCompare() throws IOException{
         StringBuilder result = new StringBuilder();
         URL url = new URL("https://api.github.com/repos/Exiua/LTSaveEd/releases/latest");
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
@@ -170,5 +171,28 @@ public class Debug {
             }
         }
         System.out.println("v0.1.0".compareTo(test));
+        System.out.println(compareVersions("v1.10.0", "v1.9.0"));
+    }
+
+    public static int compareVersions(String version1, String version2){
+        String[] v1Arr = version1.replace("v", "").split("\\.");
+        String[] v2Arr = version2.replace("v", "").split("\\.");
+        int v1Int;
+        int v2Int;
+        for(int i = 0; i < 3; i++){
+            v1Int = Integer.parseInt(v1Arr[i]);
+            v2Int = Integer.parseInt(v2Arr[i]);
+            if(v1Int > v2Int){
+                return 1;
+            }
+            else if(v1Int < v2Int){
+                return -1;
+            }
+        }
+        return 0; //Version strings must be equal at this point
+    }
+
+    public static void main(String[] args) throws IOException {
+        versionCompare();
     }
 }
