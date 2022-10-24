7ac26d1f28c59d61c8900a1d2286e1099c0180a8
Exiua <master6969697@gmail.com>
2022-10-23 23:40:39 -0500

    Updated version

--- src/main/java/LTSaveEd/LTSaveEd.java
+++ src/main/java/LTSaveEd/LTSaveEd.java
@@ -14,7 +14,7 @@ public class LTSaveEd extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        String version = "v1.3.1";
+        String version = "v1.3.2";
 
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/LTSaveEd.fxml"));
         Parent root = loader.load();
