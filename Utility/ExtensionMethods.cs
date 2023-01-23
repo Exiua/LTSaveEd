7f6edded54de277b79b867a2efa73a42f352fe49
Exiua <master6969697@gmail.com>
2023-01-23 08:48:19 -0600

    Created ExtensionMethods

---
+++ Utility/ExtensionMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace LTSaveEd.Utility;
+
+public static class ExtensionMethods
+{
+    public static void Print<T>(this IEnumerable<T> array)
+    {
+        var output = array.Aggregate("[", (current, el) => current + $"{el}, ");
+        output = string.Concat(output.AsSpan(0, output.Length - 2), "]");
+        Debug.WriteLine(output);
+    }
+}
