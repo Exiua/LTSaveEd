4235a52fe117b8e2177097a27822c23ea24edf4d
Exiua <master6969697@gmail.com>
2023-01-23 08:48:48 -0600

    Added file loading and other bindings

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,38 @@
-﻿namespace LTSaveEd.ViewModels;
+﻿using System.Diagnostics;
+using LTSaveEd.Models;
+using LTSaveEd.Views;
+using LTSaveEd.Utility;
+using ReactiveUI;
 
-public class MainWindowViewModel : ViewModelBase
+namespace LTSaveEd.ViewModels;
+
+public class MainWindowViewModel : ReactiveObject
 {
-    public string Greeting => "Welcome to Avalonia!";
+    public static MainWindow Window { private get; set; } = null!;
+
+    private static SaveFile SaveFile => SaveFile.savefile;
+
+    public CoreTabViewModel CoreTabViewModel { get; }
+
+    public MainWindowViewModel()
+    {
+        CoreTabViewModel = new CoreTabViewModel();
+    }
+    
+    public async void LoadFile()
+    {
+        var filepath = await Window.GetFilepath();
+        if (filepath is null)
+        {
+            return;
+        }
+
+        SaveFile.LoadData(filepath);
+        LoadData();
+    }
+
+    private void LoadData()
+    {
+        CoreTabViewModel.PopulateTab();
+    }
 }
--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,4 +1,8 @@
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
 using Avalonia.Controls;
+using LTSaveEd.ViewModels;
 
 namespace LTSaveEd.Views;
 
@@ -6,6 +10,28 @@ public partial class MainWindow : Window
 {
     public MainWindow()
     {
+        MainWindowViewModel.Window = this;
         InitializeComponent();
+    }
+
+    public async Task<string?> GetFilepath()
+    {
+        var fileExtensions = new List<string> { "xml" };
+        var extensionFilter = new FileDialogFilter { Extensions = fileExtensions };
+        var dialog = new OpenFileDialog
+        {
+            AllowMultiple = false,
+            Filters = new List<FileDialogFilter> { extensionFilter }
+        };
+        var result = await dialog.ShowAsync(this);
+
+        if (result is null)
+        {
+            Debug.WriteLine("Filepath: null");
+            return null;
+        }
+        
+        Debug.WriteLine("Filepath: " + string.Join(" ", result));
+        return string.Join(" ", result);
     }
 }
