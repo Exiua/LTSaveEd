c5907e2319be862f1de44e5554274e4f7749e0b5
Exiua <master6969697@gmail.com>
2023-01-23 08:47:49 -0600

    Created TabViewModels

---
+++ ViewModels/CoreTabViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using ReactiveUI;
+
+namespace LTSaveEd.ViewModels;
+
+public class CoreTabViewModel : TabViewModel
+{
+    private string _characterId = "";
+    public string CharacterId
+    {
+        get => _characterId;
+        set => this.RaiseAndSetIfChanged(ref _characterId, value);
+    }
+
+    public override void PopulateTab()
+    {
+        PopulateCharacterComboBox();
+        CharacterId = SaveFile.GetDescendantByName(SaveFile.currentCharacter, "id").FirstAttribute?.Value ?? "NULL";
+    }
+    
+    private void PopulateCharacterComboBox()
+    {
+        var npcs = SaveFile.GetNpcList();
+        //npcs.Print();
+        Debug.WriteLine("Populated Character ComboBox");
+    }
+}
---
+++ ViewModels/TabViewModel.cs
@@ -0,0 +1,12 @@
+﻿using LTSaveEd.Models;
+using LTSaveEd.Views;
+using ReactiveUI;
+
+namespace LTSaveEd.ViewModels;
+
+public abstract class TabViewModel : ReactiveObject
+{
+    protected static SaveFile SaveFile => SaveFile.savefile;
+    
+    public abstract void PopulateTab();
+}
