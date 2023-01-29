0625c2e96432c3199a1aa9bad64ee5dd4ad80229
Exiua <master6969697@gmail.com>
2023-01-28 18:30:59 -0600

    New Exception

---
+++ Exceptions/TagNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace LTSaveEd.Exceptions;
+
+public class TagNotFoundException : SaveEditorException
+{
+    public TagNotFoundException() { }
+    public TagNotFoundException(string tagName) : base($"Tag not found: {tagName}") { }
+    public TagNotFoundException(string tagName, Exception inner) : base($"Tag not found: {tagName}", inner) { }
+}
---
+++ Exceptions/ValueNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace LTSaveEd.Exceptions;
+
+public class ValueNotFoundException : SaveEditorException
+{
+    public ValueNotFoundException() { }
+    public ValueNotFoundException(string tagName) : base($"Attribute value not found for tag: {tagName}") { }
+    public ValueNotFoundException(string tagName, Exception inner) : base($"Attribute value not found for tag: {tagName}", inner) { }
+}
