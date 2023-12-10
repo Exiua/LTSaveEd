e2d3c020f89dcdaf77389f9a9dec11fd1603f3ac
Exiua <master6969697@gmail.com>
2023-12-10 00:49:48 -0600

    Working on implementing spell pages

--- Models/CharacterData/Character.cs
+++ Models/CharacterData/Character.cs
@@ -10,6 +10,7 @@ public class Character(XElement characterNode)
     public Attributes Attributes { get; } = new(characterNode.Element("attributes")!);
     public Fetishes Fetishes { get; } = new(characterNode.Element("fetishes")!);
     public Perks Perks { get; } = new(characterNode.Element("perks")!);
+    public Spells Spells { get; } = new(characterNode.Element("knownSpells")!, characterNode.Element("spellUpgrades")!, characterNode.Element("spellUpgradePoints")!);
     public Family Family { get; } = new(characterNode.Element("family")!);
 
     public bool IsPlayer => Core.Id.Value == "PlayerCharacter";
--- Models/CharacterData/Spells.cs
+++ Models/CharacterData/Spells.cs
@@ -1,6 +1,22 @@
-﻿namespace LTSaveEd.Models.CharacterData;
+﻿using System.Xml.Linq;
+using LTSaveEd.Models.CharacterData.SpellData;
+
+namespace LTSaveEd.Models.CharacterData;
 
 public class Spells
 {
-    
+    public EarthSpells Earth { get; }
+    public WaterSpells Water { get; }
+    public FireSpells Fire { get; }
+    public AirSpells Air { get; }
+    public ArcaneSpells Arcane { get; }
+
+    public Spells(XElement knownSpellsNode, XElement spellUpgradesNode, XElement spellUpgradePointsNode)
+    {
+        Earth = new EarthSpells(knownSpellsNode, spellUpgradesNode, spellUpgradePointsNode);
+        Water = new WaterSpells(knownSpellsNode, spellUpgradesNode, spellUpgradePointsNode);
+        Fire = new FireSpells(knownSpellsNode, spellUpgradesNode, spellUpgradePointsNode);
+        Air = new AirSpells(knownSpellsNode, spellUpgradesNode, spellUpgradePointsNode);
+        Arcane = new ArcaneSpells(knownSpellsNode, spellUpgradesNode, spellUpgradePointsNode);
+    }
 }
