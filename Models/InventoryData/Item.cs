1f7b83c9d4ab2ba2cfe738d376605b1a093af56b
Exiua <master6969697@gmail.com>
2023-12-15 23:42:10 -0600

    Working on inventory support

---
+++ Models/InventoryData/Item.cs
@@ -0,0 +1,15 @@
+﻿using System.Xml.Linq;
+using LTSaveEd.Models.XmlData;
+
+namespace LTSaveEd.Models.InventoryData;
+
+public class Item : InventoryElement
+{
+
+    public XmlAttribute<string> Color { get; }
+    
+    public Item(XElement itemNode) : base(itemNode)
+    {
+        Color = new XmlAttribute<string>(itemNode.Attribute("colour")!);
+    }
+}
