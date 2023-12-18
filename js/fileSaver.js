799ee4385456786174c1b67d5bdbb3f9f9bce6d5
Exiua <52053026+Exiua@users.noreply.github.com>
2023-12-18 20:08:19 +0000

    Deploying to gh-pages from @ Exiua/LTSaveEd@3ac24c987131ca439f2293e55e5cf6deee847cd5 🚀

---
+++ js/fileSaver.js
@@ -0,0 +1,9 @@
+﻿function saveAsFile(filename, byteBase64) {
+    const link = document.createElement('a');
+    link.download = filename;
+    const blob = new Blob([byteBase64], { type: 'application/octet-stream' });
+    link.href = window.URL.createObjectURL(blob);
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
