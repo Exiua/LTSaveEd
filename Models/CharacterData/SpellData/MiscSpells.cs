73e381b321453f982f7fd46196774145afd21abd
Exiua <master6969697@gmail.com>
2025-02-18 15:13:51 -0600

    add misc spells and update NullableSpell to handle nullable knownSpells as well

---
+++ Models/CharacterData/SpellData/MiscSpells.cs
@@ -0,0 +1,25 @@
+﻿using System.Xml.Linq;
+
+namespace LTSaveEd.Models.CharacterData.SpellData;
+
+public class MiscSpells : ElementalSpells
+{
+    public NullableSpell WitchsSeal { get; }
+    public NullableSpell WitchsCharm { get; }
+    public NullableSpell SirensCall { get; }
+    public NullableSpell LightningDischarge { get; }
+    public NullableSpell LightningOvercharge { get; }
+    public NullableSpell ChainLightning { get; }
+    public NullableSpell LightningSuperbolt { get; }
+    
+    public MiscSpells(XElement knownSpellsNode, XElement spellUpgradesNode) : base(knownSpellsNode, spellUpgradesNode)
+    {
+        WitchsSeal = new NullableSpell(knownSpellsNode, "WITCH_SEAL", nodeName: "spell");
+        WitchsCharm = new NullableSpell(knownSpellsNode, "WITCH_CHARM", nodeName: "spell");
+        SirensCall = new NullableSpell(knownSpellsNode, "SIRENS_CALL", nodeName: "spell");
+        LightningDischarge = new NullableSpell(knownSpellsNode, "LIGHTNING_SPHERE_DISCHARGE", nodeName: "spell");
+        LightningOvercharge = new NullableSpell(knownSpellsNode, "LIGHTNING_SPHERE_OVERCHARGE", nodeName: "spell"); 
+        ChainLightning = new NullableSpell(knownSpellsNode, "CHAIN_LIGHTNING", nodeName: "spell");
+        LightningSuperbolt = new NullableSpell(knownSpellsNode, "LIGHTNING_SUPERBOLT", nodeName: "spell");
+    }
+}
